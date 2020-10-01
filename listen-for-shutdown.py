e85ae84fcb5c9ac4f8f09cf126cb0fad901be3c5
Nicholas Wilde <ncwilde43@gmail.com>
2020-09-30 22:06:56 -0700

    Added constants and created main method

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
-from gpiozero import Button
-from signal import pause
-import subprocess
+from gpiozero   import Button
+from signal     import pause
+from subprocess import call
+import os
+
+PIN_NO = 18
+HOLD_TIME=2
 
 def longpress():
-    subprocess.call(['shutdown', '-h', 'now'], shell=False)
+    print("Shutting down")
+    os.system('sudo turn-off-nodes.sh')
+    call(['shutdown', '-h', 'now'], shell=False)
 
-button = Button(24, hold_time=2)
-button.when_held = longpress
-pause()
+def main():
+    button = Button(PIN_NO, hold_time=HOLD_TIME)
+    button.when_held = longpress
+    print("Staring listen for shutdown")
+    pause()
+
+if __name__ == "__main__":
+    main()
